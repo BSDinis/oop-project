a431df8a548d405b949499ffb8f95657cc15ee88
BSDinis <baltasardinis@hotmail.com>
2018-11-14 01:32:41 +0000

    implemented DoOpen

--- sth-app/src/sth/app/main/DoOpen.java
+++ sth-app/src/sth/app/main/DoOpen.java
@@ -7,34 +7,38 @@ import java.lang.ClassNotFoundException;
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.Input;
 import sth.SchoolManager;
+import sth.exceptions.ImportFileException;
 
 /**
  * 4.1.1. Open existing document.
  */
 public class DoOpen extends Command<SchoolManager> {
 
-
-  //private InputString _filename;
   /**
    * @param receiver
    */
   public DoOpen(SchoolManager receiver) {
     super(Label.OPEN, receiver);
-    // _filename = new InputString(Message.openFile()); // FIXME
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
-    /*
+    Form f = new Form();
+    InputString filename = new InputString(f, Message.openFile());
+    f.parse();
     try {
-      //FIXME implement command
-    } catch (FileNotFoundException fnfe) {
-      _display.popup(Message.fileNotFound());
-    } catch (ClassNotFoundException | IOException e) {
-      e.printStackTrace();
+      _receiver.importFile(filename.value());
     }
-    */
+    catch (FileNotFoundException e) {
+      _display.popup(fileNotFound(filename.value());
+    }
+    catch (ImportFileException e) {
+      _display.popup(e.getMessage());
+    }
+    catch (UnsupportedOperationException e) {
+      _display.popup("Operação não suportada");
+    }
   }
 
 }
