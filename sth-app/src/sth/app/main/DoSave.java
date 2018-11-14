3e565a0c007fb7c300b1d426f6ce1efddd925850
BSDinis <baltasardinis@hotmail.com>
2018-11-14 01:47:34 +0000

    updated DoSave

--- sth-app/src/sth/app/main/DoSave.java
+++ sth-app/src/sth/app/main/DoSave.java
@@ -10,21 +10,42 @@ import sth.SchoolManager;
  * 4.1.1. Save to file under current name (if unnamed, query for name).
  */
 public class DoSave extends Command<SchoolManager> {
-  //private InputString _filename;
-
-
   /**
    * @param receiver
    */
   public DoSave(SchoolManager receiver) {
     super(Label.SAVE, receiver);
-    //_filename = new InputString(Message.saveAs()); // FIXME
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
-    //FIXME implement command
+    String filename = _receiver.getFilename();
+
+    if (filename == null) {
+      Form f = new Form();
+      Input<String> inputFilename = f.addStringInput(Message.newSaveAs());
+      f.parse();
+      filename = inputFilename.value();
+    }
+
+    try {
+      _receiver.saveToFile(filename);
+    }
+    /*
+    catch (FileNotFoundException e) {
+      _display.popup(Message.fileNotFound(filename.value()));
+    }
+    FIXME ????
+    */
+    catch (ImportFileException e) {
+      _display.popup(e.getMessage());
+    }
+    catch (UnsupportedOperationException e) {
+      _display.popup("Operação não suportada");
+    }
+
+
   }
 
 }
