2d57e133725e4d053091717d1559a5e77dc8d075
BSDinis <baltasardinis@hotmail.com>
2018-11-14 00:28:32 +0000

    added new exception

---
+++ sth-core/src/sth/exceptions/NoPersonLoggedInException.java
@@ -0,0 +1,7 @@
+package sth.exceptions;
+
+/** Exception thrown when the no one is logged in. */
+public class NoPersonLoggedInException extends Exception {
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 201811140027L;
+}
