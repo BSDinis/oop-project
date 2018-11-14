48c983c07e173804fc3c89dd8bd0285c80541aed
BSDinis <baltasardinis@hotmail.com>
2018-11-14 00:54:05 +0000

    added dummy student

---
+++ sth-core/src/sth/Student.java
@@ -0,0 +1,15 @@
+package sth;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Person implementation.
+ */
+public class Student extends Person {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 201811140053L;
+  // FIXME
+
+}
