3242b4d30df4cb9a88de6e715318d18ce26fd4a6
BSDinis <baltasardinis@hotmail.com>
2018-11-14 00:49:34 +0000

    added person

---
+++ sth-core/src/sth/Person.java
@@ -0,0 +1,16 @@
+package sth;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Person implementation.
+ */
+public class Person implements Serializable {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 201810051538L;
+
+  // FIXME
+
+}
