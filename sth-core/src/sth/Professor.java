3c154e2b7d349a6fac1bb00d91c47f076979a7c1
BSDinis <baltasardinis@hotmail.com>
2018-11-15 12:51:32 +0000

    added constructors

--- sth-core/src/sth/Professor.java
+++ sth-core/src/sth/Professor.java
@@ -6,10 +6,13 @@ import java.io.Serializable;
 /**
  * Person implementation.
  */
-public class Professor extends Person {
+public class Professor 
+  extends Person 
+  implements Serializable {
+
+  Professor(String n, String pN, int id) { super(n, pN, id); }
 
   /** Serial number for serialization. */
   private static final long serialVersionUID = 201811151230L;
-  // FIXME
 
 }
--- sth-core/src/sth/Student.java
+++ sth-core/src/sth/Student.java
@@ -6,7 +6,11 @@ import java.io.Serializable;
 /**
  * Person implementation.
  */
-public class Student extends Person {
+public class Student 
+  extends Person 
+  implements Serializable {
+
+  Student(String n, String pN, int id) { super(n, pN, id); }
 
   /** Serial number for serialization. */
   private static final long serialVersionUID = 201811140053L;
