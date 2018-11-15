8bb04b5e41c11aef968c1f1d9e467722b686d56c
BSDinis <baltasardinis@hotmail.com>
2018-11-15 12:42:23 +0000

    added professor class

---
+++ sth-core/src/sth/Professor.java
@@ -0,0 +1,15 @@
+package sth;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Person implementation.
+ */
+public class Professor extends Person {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 201811151230L;
+  // FIXME
+
+}
