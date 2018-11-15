93a8ac28f2723f7b1a794c5e2fd418e112beb37b
BSDinis <baltasardinis@hotmail.com>
2018-11-15 12:27:15 +0000

    added unsupported operations

--- sth-core/src/sth/Survey.java
+++ sth-core/src/sth/Survey.java
@@ -2,15 +2,45 @@ package sth;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.lang.UnsupportedOperationException;
 
 public class Survey {
-  private boolean _finished = false;
-  private boolean _open;
   private Map<Student, IncompleteSurveyResponse> _responses = new TreeMap<Student, IncompleteSurveyResponse>();
 
   Survey(boolean isProjectOpen) {
     if (isProjectOpen) open = false;
     else open = true;
+  }
+
+  public void close() 
+    throws UnsupportedOperationException {
+    // FIXME
+    
+    throw new UnsupportedOperationException();
+  }
+
+  public void finish() 
+    throws UnsupportedOperationException {
+    // FIXME
+    
+    throw new UnsupportedOperationException();
+  }
+
+   
+  public void open() 
+    throws UnsupportedOperationException {
+    // FIXME
+    
+    throw new UnsupportedOperationException();
+  }
+
+   
+   
+  public void addResponse(Student s, int hours, String comment) 
+    throws UnsupportedOperationException {
+    // FIXME
+    
+    throw new UnsupportedOperationException();
   }
 
   public String toString() { return "<<Survey :: to implement>>"; }
