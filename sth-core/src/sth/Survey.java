c4e902848077b485518d4afc90fd75ebea70447b
BSDinis <baltasardinis@hotmail.com>
2018-11-15 12:23:57 +0000

    added responses

--- sth-core/src/sth/Survey.java
+++ sth-core/src/sth/Survey.java
@@ -1,6 +1,17 @@
 package sth;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Survey {
-  // FIXME
+  private boolean _finished = false;
+  private boolean _open;
+  private Map<Student, IncompleteSurveyResponse> _responses = new TreeMap<Student, IncompleteSurveyResponse>();
+
+  Survey(boolean isProjectOpen) {
+    if (isProjectOpen) open = false;
+    else open = true;
+  }
+
   public String toString() { return "<<Survey :: to implement>>"; }
 }
