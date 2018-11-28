3e0befa7cf741e4faa51481960c7d28474c0fa09
BSDinis <baltasardinis@hotmail.com>
2018-11-28 20:48:34 +0000

    cleaned up Parser

--- sth-core/src/sth/PersonParser.java
+++ sth-core/src/sth/PersonParser.java
@@ -20,6 +20,7 @@ import sth.exceptions.EnrollmentLimitReachedException;
 import sth.exceptions.DuplicatePersonException;
 import sth.exceptions.DuplicateCourseException;
 
+
 class PersonParser implements Serializable {
   private LinkedList<List<String>> _lines = new LinkedList<List<String>>();
   BufferedReader _in;
@@ -43,8 +44,15 @@ class PersonParser implements Serializable {
     return true;
   }
 
+
+  /**
+   * Make way for a new person
+   */
   private void clear() { _lines = new LinkedList<List<String>>(); }
 
+  /**
+   * Generates a person; redirects to the correct call
+   */
   private void generatePerson() 
     throws BadEntryException {
     List<String> header = _lines.getFirst();
@@ -62,60 +70,57 @@ class PersonParser implements Serializable {
     String phoneNumber = header.get(2);
     String name = header.get(3);
 
-    if (isRepresentativeDescriptor(header.get(0)) || isStudentDescriptor(header.get(0))) {
-      generateStudent(header.get(0), _lines.subList(1, _lines.size()), id, phoneNumber, name);
+    if (isStudentDescriptor(header.get(0))) {
+      generateStudent(_lines.subList(1, _lines.size()), id, phoneNumber, name);
+    }
+    else if (isRepresentativeDescriptor(header.get(0))) {
+      generateRepresentative(_lines.subList(1, _lines.size()), id, phoneNumber, name);
     }
     else if (isProfessorDescriptor(header.get(0))) {
-      generateProfessor(header.get(0), _lines.subList(1, _lines.size()), id, phoneNumber, name);
+      generateProfessor(_lines.subList(1, _lines.size()), id, phoneNumber, name);
     }
     else if (isStafferDescriptor(header.get(0))) {
-      generateStaffer(header.get(0), _lines.subList(1, _lines.size()), id, phoneNumber, name);
+      generateStaffer(_lines.subList(1, _lines.size()), id, phoneNumber, name);
     }
   }
 
-  private void generateStaffer(String descriptor, Collection<List<String>> lines,
+  /**
+   * Add staffer to school
+   */
+  private Staffer generateStaffer(Collection<List<String>> lines,
       int id, String phoneNumber, String name) 
     throws BadEntryException {
 
+    Staffer s;
     if (lines.size() != 0) 
-      throw new BadEntryException(reconstruct(descriptor, id, phoneNumber, name));
+      throw new BadEntryException(reconstruct(_lines.getFirst()));
     
     try {
-      _school.addStaffer(new Staffer(name, phoneNumber, id, _school));
+      s = new Staffer(name, phoneNumber, id, _school);
+      _school.addStaffer(s);
     } catch (DuplicatePersonException e) {
-      throw new BadEntryException(reconstruct(descriptor, id, phoneNumber, name));
+      throw new BadEntryException(reconstruct(_lines.getFirst())); 
     }
+
+    return s;
   }
 
 
-  private Course generateCourseIfNeeded(String courseName) {
-    try {
-      return _school.addCourse(new Course(courseName, 7)); // magic number: max number of representatives
-    }
-    catch (DuplicateCourseException e) {
-      return _school.getCourseByName(courseName);
-    }
-  }
 
-
-  private Discipline generateDisciplineIfNeeded(Course c, String disciplineName) {
-    if (c.hasDiscipline(disciplineName)) {
-      return c.getDiscipline(disciplineName);
-    }
-    // magic number: the Ultimate Question of Life, The Universe and Everything is: what is the maximum number (in hex, obviously) of students in a hypothetical class of the OOP project; those philosophers would be happy
-    return c.addDiscipline(new Discipline(disciplineName, 0x42, c));
-  }
-
-  private void generateProfessor(String descriptor, List<List<String>> lines,
+  /**
+   * Add professor to school
+   */
+  private Professor generateProfessor(List<List<String>> lines,
       int id, String phoneNumber, String name) 
     throws BadEntryException {
 
 
     Professor p;
+
     try {
       p = _school.addProfessor(new Professor(name, phoneNumber, id, _school));
     } catch (DuplicatePersonException e) {
-      throw new BadEntryException(reconstruct(descriptor, id, phoneNumber, name));
+      throw new BadEntryException(reconstruct(_lines.getFirst()));
     }
 
     for (List<String> fields : lines) {
@@ -132,9 +137,35 @@ class PersonParser implements Serializable {
         throw new BadEntryException(reconstruct(fields));
       }
     }
+
+    return p;
   }
 
-  private void generateStudent(String descriptor, List<List<String>> lines,
+
+  /**
+   * Add a representative to the school
+   */
+  private Student generateRepresentative(List<List<String>> lines,
+      int id, String phoneNumber, String name) 
+    throws BadEntryException {
+
+    Student s = generateStudent(lines, id, phoneNumber, name);
+    Course c = s.getCourse();
+
+    try {
+      c.electRepresentative(s);
+    }
+    catch (TooManyRepresentativesException e) {
+      throw new BadEntryException(reconstruct(_lines.getFirst())); 
+    }
+
+    return s;
+  }
+
+  /**
+   * Add a student to the school
+   */
+  private Student generateStudent(List<List<String>> lines,
       int id, String phoneNumber, String name) 
     throws BadEntryException {
 
@@ -145,46 +176,38 @@ class PersonParser implements Serializable {
     try {
       s = _school.addStudent(new Student(name, phoneNumber, id, _school));
     } catch (DuplicatePersonException e) {
-      throw new BadEntryException(reconstruct(descriptor, id, phoneNumber, name));
+      throw new BadEntryException(reconstruct(_lines.getFirst()));
     }
 
     for (List<String> fields : lines) {
       String courseName = fields.get(0).substring(2);
       String disciplineName = fields.get(1);
-
       Course c = generateCourseIfNeeded(courseName);
 
-      if (s.getCourse() == null) {
+      if (s.getCourse() == null)
         s.enrollInCourse(c);
-
-        if (isRepresentativeDescriptor(descriptor)) {
-          try {
-            c.electRepresentative(s);
-          }
-          catch (TooManyRepresentativesException e) {
-            throw new BadEntryException(reconstruct(fields)); 
-          }
-        }
-      }
-      else if (!courseName.equals(s.getCourse().name())) {
+      else if (!courseName.equals(s.getCourse().name()))
         throw new BadEntryException(reconstruct(fields));
-      }
 
       Discipline d = generateDisciplineIfNeeded(c, disciplineName);
       
       try {
         d.enrollStudent(s);
       }
-      catch (StudentAlreadyEnrolledException 
-          | AlienStudentException 
-          | EnrollmentLimitReachedException 
-          | DisciplineLimitReachedException e) {
+      catch (StudentAlreadyEnrolledException | AlienStudentException 
+           | EnrollmentLimitReachedException | DisciplineLimitReachedException e) {
         throw new BadEntryException(reconstruct(fields));
       }
     }
+
+    return s;
   }
 
-  // for throwing meaningful exceptions
+  /**
+   * Reconstruct the line, so that the exceptions showcase the original line
+   *
+   * @param fields
+   */
   private String reconstruct(List<String> fields) {
     if (fields.size() == 0) return "";
 
@@ -195,13 +218,54 @@ class PersonParser implements Serializable {
 
     return result;
   }
-
-  // special case
+  /**
+   * Alternative call
+   */
   private String reconstruct(String descriptor, int id, String phoneNumber, String name) {
     return reconstruct(Arrays.asList(descriptor, String.valueOf(id), phoneNumber, name));
   }
 
+
   /**
+   * Generate a course if needed
+   *
+   * @param courseName
+   * @return bool
+   */
+  private Course generateCourseIfNeeded(String courseName) {
+    try {
+      // magic number: max number of representatives
+      return _school.addCourse(new Course(courseName, 7)); 
+    }
+    catch (DuplicateCourseException e) {
+      return _school.getCourseByName(courseName);
+    }
+  }
+
+
+  /**
+   * Generate a discipline if needed
+   *
+   * @param course
+   * @param disciplineName
+   * @return bool
+   */
+  private Discipline generateDisciplineIfNeeded(Course c, String disciplineName) {
+    if (c.hasDiscipline(disciplineName)) {
+      return c.getDiscipline(disciplineName);
+    }
+    /* magic number: the Ultimate Question of Life, The Universe and
+     * Everything is: what is the maximum number (in hex, obviously)
+     * of students in a hypothetical class of the OOP project; 
+     * those philosophers would be happy */
+    return c.addDiscipline(new Discipline(disciplineName, 0x42, c));
+  }
+
+
+
+  /**
+   * Parse through lines getting the fields
+   *
    * @param  String : first line
    * @throws BadEntryException
    * @return String : last line read
@@ -222,6 +286,13 @@ class PersonParser implements Serializable {
     return line;
   }
 
+  /**
+   * Parse through a new line
+   * Can verify whether it is the first line or not
+   *
+   * @param line String for the line
+   * @return success of acceptance
+   */
   private boolean acceptLine(String line) throws BadEntryException {
     List<String> fields = new ArrayList<String>(Arrays.asList(line.split("\\|")));
 
@@ -242,12 +313,19 @@ class PersonParser implements Serializable {
     return true;
   }
 
-  // checks if the fields are good for a person header line
+  /**
+   * Check if fields correspond to a header line
+   *
+   * @param fields Line separated by spaces
+   */
   private boolean goodHeaderLine(List<String> fields) {
     if (fields.size() != 4) return false;
 
-    if (!fields.get(0).equals("FUNCIONÁRIO") && !fields.get(0).equals("DELEGADO")
-        && !fields.get(0).equals("ALUNO") && !fields.get(0).equals("DOCENTE")) {
+    String descriptor = fields.get(0);
+    if (   !isRepresentativeDescriptor(descriptor)
+        && !isProfessorDescriptor(descriptor)
+        && !isStudentDescriptor(descriptor)
+        && !isStafferDescriptor(descriptor) ) {
       return false;
     }
 
@@ -257,7 +335,11 @@ class PersonParser implements Serializable {
     return true;
   }
 
-  // checks if the fields are good for a person header line
+  /** 
+   * Checks if the fields are good for a person header line
+   *
+   * @param fields Line separated by spaces
+   */
   private boolean goodBodyLine(List<String> fields) {
     if (fields.size() != 2) return false;
 
