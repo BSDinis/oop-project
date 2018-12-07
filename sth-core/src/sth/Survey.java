d516ae506a7f250d6cab9852375a09e3efc456eb
BSDinis <baltasardinis@hotmail.com>
2018-12-07 01:45:38 +0000

    added verification

--- sth-core/src/sth/Survey.java
+++ sth-core/src/sth/Survey.java
@@ -116,13 +116,9 @@ public class Survey
     void finish() {}
     void cancel() throws FinishedSurveyException { throw new FinishedSurveyException(disciplineName(), projectName());}
     String print(SurveyPrinter p) { return p.print(this); }
-    public int minHours() { return _minHours; }
-    public int maxHours() { return _maxHours; }
-    public int medHours() { 
-      if (responsesNumber() == 0) 
-        return 0; // this will never happen
-      return _sumHours / responsesNumber(); 
-    }
+    public int minHours() { return (responsesNumber() > 0 ) ? _minHours : 0; }
+    public int maxHours() { return (responsesNumber() > 0 ) ? _maxHours : 0; }
+    public int medHours() { return (responsesNumber() > 0 ) ? _sumHours/responsesNumber() : 0; }
     SurveyNotification genNotification() { return new SurveyFinishNotification(Survey.this); }
     public int submissionNumber() { return _parentProject.submissionNumber(); }
     public int responsesNumber() { return _responses.size(); }
